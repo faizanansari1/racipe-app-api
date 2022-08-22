7a55fc1f43a0200e930a3f1ec57021768a4b676b
faizanansari1 <faizanshaikhansari@gmail.com>
2022-08-22 23:25:18 +0500

    write simple unit test  SimpleTestCase..

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Simple tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(2, 6)
+
+        self.assertEqual(res, 8)
